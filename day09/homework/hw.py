9aa5020ebc83e70d7bf4c28f5da649b917a19c60
zuradavitadze <zukaczuka@gmail.com>
2024-10-26 15:21:51 +0400

    third commit

---
+++ day09/homework/hw.py
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<tittle>chemi davaleba</tiitle>
+<style> 
+body {text-align: center}
+</style>
+</head>
+<body>
+>/body>
+</html>
+
+<h1>es aris chemi pirveli satauri</h1>
+
+<h2>es aris meore satauri</h2>
+<p>es aris paragrafi.</p>
+
+<h2>chemi sayvareli feri</h2>
+<p>chemi sayvareli feri aris lurji.</p>
+
+<h3>chemi sayvareli sporti aris</h3>
+<p>chemi sayvareli sportia fexburti.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day15/homework/hw.py
@@ -0,0 +1,19 @@
+number = int(input("sheiyvanet ricxvi:"))
+if number < 10:
+print("ricxvi 10-ze naklebia")
+else:
+print("ricxvi 10-ze metia an tolia")
+
+number = int(input("sheiyvanet ricxvi:"))
+if number % == 0 :
+print("ricxvi luwia")
+else:
+print("ricxvi kentia")
+
+number = int(input("sheiyvanet ricxvi:"))
+if number > 0:
+print("ricxvi dadebitia")
+elif number < 0:
+print("ricxvi uaryofitia")
+else:
+print("ricxvi nulia")
