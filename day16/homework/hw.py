920618ec0b592b27934a5c4abb80ae9b66b16d43
zuradavitadze <zukaczuka@gmail.com>
2024-10-31 15:33:35 +0400

    third commit

---
+++ day16/homework/hw.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<tittle>welcome to my website</tittle>
+</head>
+<body>
+<h1>მოგესალმებიტ ჩემ ვებსაიტზე!</h1>
+<p>ეს არის გვერდი ფეხბურთზე მე ძალიან მიყვარს ფეხბურთი.</p>
+<p>გმადლობთ, რომ შემოხვედით!</p>
+</body>
+</html>
+
+
