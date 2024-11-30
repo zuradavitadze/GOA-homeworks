07d05f01b1453ce45124c5a8705c6eaf41b963c8
zuradavitadze <zukaczuka@gmail.com>
2024-11-30 10:16:33 +0400

    first commit

---
+++ day25/homework/hw.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>მაშა და მედვედი</title>
+</head>
+<body>
+    <h1>მაშა და მედვედი</h1>
+    <p>ეს არის ვებსაიტი მაშა და მედვედის შესახებ.</p>
+    <img src="masha.jpg" alt="მაშა">
+</body>
+</html>
