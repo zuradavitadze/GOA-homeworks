c5015bc4bc6f27845e9ab279c360b244a8f95b8a
zuradavitadze <zukaczuka@gmail.com>
2024-12-08 20:53:31 +0400

    first commit

---
+++ day27/homework/hw.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>jQuery სტილიზაცია</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="script.js"></script>
+</head>
+<body>
+  <p id="myParagraph1" class="special">ეს არის პირველი პარაგრაფი</p>
+  <p id="myParagraph2" class="special">ეს არის მეორე პარაგრაფი</p>
+</body>
+</html>
